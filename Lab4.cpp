eb9b30960676db4a2733e42990da48724baf4939
LordVillain <danyailyinchyk2005@gmail.com>
2023-10-03 15:57:01 +0300

    Create Lab4.cpp

---
+++ Lab4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int tipa_strcspn(const char *string, const char *strCharSet) {
+    int count = 0;
+    while (*string) {
+        const char *ch = strCharSet;
+        bool a=0;
+        while (*ch) {
+            if (*string == *ch) {
+                a = 1;
+                break;
+            }
+            ch++;
+        }
+        string++;
+        if (a == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    const char *string = "rablvoero";
+    const char *strCharSet = "eou";
+
+    int result = tipa_strcspn(string, strCharSet);
+
+    std::cout << "strCharSet: " << result << std::endl;
+
+    return 0;
+}
